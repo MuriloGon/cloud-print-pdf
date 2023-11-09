b6b1085da1ea1954470e30b8b588a6da4905036b
Murilo Gonçalves <murilogoncalvesdev@gmail.com>
2023-11-09 15:14:15 -0300

    feat: control ws connectio with config file values

--- src/bin/cloud_file_manager.rs
+++ src/bin/cloud_file_manager.rs
@@ -1,21 +1,25 @@
 use cloud_print::logger;
 use log::{error, info};
 use std::error::Error;
-use websocket::{header::Headers, ClientBuilder};
+use websocket::ClientBuilder;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let _ = logger::setup_logger(String::from("cloud_file_man.log"));
 
     let app_config = cloud_print::config::AppConfig::from_config_file();
+    let url = format!(
+        "{}?eventId={}&context={}&pwd={}",
+        &app_config.ws_url,
+        &app_config.ws_context_id,
+        &app_config.ws_context_name,
+        &app_config.ws_context_pwd
+    );
+    info!("Websocket url={}", &url);
 
-    let mut headers = Headers::new();
-    headers.append_raw("x-context-id", Vec::from(app_config.context_id.clone()));
-    headers.append_raw("x-context-name", Vec::from(app_config.context_name.clone()));
-
-    let mut builder = match ClientBuilder::new(&app_config.websocket_url) {
+    let mut builder = match ClientBuilder::new(&url) {
         Ok(x) => {
-            info!("Websock Url is ok");
-            x.custom_headers(&headers)
+            info!("Websocket URL is OK");
+            x
         }
         Err(e) => {
             error!("Error parsing ws url: {:?}", e);
@@ -23,9 +27,9 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     };
 
-    let client = match builder.connect_insecure() {
+    let mut client = match builder.connect(None) {
         Ok(c) => {
-            info!("Clicent successfuly connected");
+            info!("Client successfuly connected");
             c
         }
         Err(e) => {
@@ -34,12 +38,8 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     };
 
-    let (mut receiver, mut sender) = client.split().unwrap();
-
-    for message in receiver.incoming_messages() {
+    for message in client.incoming_messages() {
         println!("{:?}", &message.unwrap());
-        // Echo the message back
-        // sender.send_message(&message.unwrap()).unwrap();
     }
 
     Ok(())
