9c2a2c5db2baf3d93f3d8f9c69738ec7d3e2aafb
Murilo Gonçalves <murilogoncalvesdev@gmail.com>
2023-11-13 00:37:00 -0300

    fix: add logg to individual bins

--- src/bin/cloud_file_manager.rs
+++ src/bin/cloud_file_manager.rs
@@ -1,9 +1,10 @@
-use cloud_print::{config::AppConfig, main_executors, dirs::Dirs};
+use cloud_print::{config::AppConfig, dirs::Dirs, logger, main_executors};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    logger::setup_logger(String::from("cloud_file_manager.log"))?;
     let app_config = AppConfig::from_config_file();
     Dirs::generate_working_dir(&app_config);
 
-    main_executors::cloud_file_manager(app_config)?;
+    main_executors::cloud_file_manager(app_config, None)?;
     Ok(())
 }
--- src/bin/printer_manager.rs
+++ src/bin/printer_manager.rs
@@ -1,9 +1,10 @@
-use cloud_print::{main_executors, config::AppConfig, dirs::Dirs};
+use cloud_print::{config::AppConfig, dirs::Dirs, logger, main_executors};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    logger::setup_logger(String::from("printer_manager.log"))?;
     let app_config = AppConfig::from_config_file();
     Dirs::generate_working_dir(&app_config);
 
-    main_executors::printer_manager(app_config)?;
+    main_executors::printer_manager(app_config, None)?;
     Ok(())
 }
