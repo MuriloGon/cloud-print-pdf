094041a6f70b3281520efd00ef4e2b10cae8bfe7
Murilo Gonçalves <murilogoncalvesdev@gmail.com>
2023-11-14 00:10:09 -0300

    fix: command args

--- src/printer_manager.rs
+++ src/printer_manager.rs
@@ -23,27 +23,35 @@ impl PrinterManager {
     }
 
     pub fn print_file(&self, message: &Message) -> Result<(), String> {
-        log::info!("Message used on current print: \n{}", message);
+        log::info!("Message used on current print: \n{}", &message);
 
-        let executable_path = Path::new(&self.app_config.printer_bin);
+        let pdf_string = message.clone().pdf_local_path.unwrap();
+
+        let pdf_path = Path::new(&pdf_string);
+
+        let executable_path = Path::new(&self.app_config.root_path)
+        .join(&self.app_config.printer_bin);
+
         let mut args: Vec<String> = Vec::new();
         args.push(String::from("-print-to"));
-        args.push(format!("\"{}\"", &self.app_config.printer_name));
+        args.push(format!("{}", &self.app_config.printer_name));
         args.push(String::from("-print-settings"));
-        args.push(format!("\"{}\"", &self.app_config.printer_settings));
-        args.push(format!("\"{}\"", message.pdf_local_path.clone().unwrap()));
+        args.push(format!("{}", &self.app_config.printer_settings));
+        args.push(format!("{}", &pdf_path.to_string_lossy().to_string()));
 
         let command_executed = format!("{} {}", &executable_path.to_string_lossy(), args.join(" "));
         info!("Command that will be executed:\n\"{}\"", command_executed);
 
         let command_result = Command::new(executable_path)
             .args(&args)
-            .spawn()
+            .output()
             .map_err(|err| err.to_string());
 
+        info!("{command_result:?}");
+
         match command_result {
-            Ok(_child) => {
-                info!("Command executed successfuly");
+            Ok(output) => {
+                info!("Command executed successfuly\n{}", std::str::from_utf8(&output.stdout).unwrap());
                 Ok(())
             }
             Err(e) => {
